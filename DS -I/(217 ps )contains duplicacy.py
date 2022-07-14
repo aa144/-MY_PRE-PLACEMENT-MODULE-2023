be568557a7e702702364f6ddfc3eea5e5fa574f3
aa144 <75486892+aa144@users.noreply.github.com>
2022-07-14 23:47:49 +0530

    BINARY SEARCH

--- DS -I/(217 ps )contains duplicacy.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        for indx in range(0, len(nums) -1):
-            if (nums[indx] == nums[indx+1]):
-                return True
-            return False
